d91064e05387883e40fba37a5fad3032452c874e
KRLiebel <79476734+KRLiebel@users.noreply.github.com>
2021-10-07 14:47:28 -0300

    Add files via upload

---
+++ Desafio02.py
@@ -0,0 +1,5 @@
+print('Olá, queremos saber mais de você.')
+data = input('Nos informe qual sua data de nascimento:')
+mes = input('Qual o mês de nascimento?')
+ano = input('E o ano?')
+print('Está é sua data de nascimento:',data, 'do mês',mes, 'de',ano)
