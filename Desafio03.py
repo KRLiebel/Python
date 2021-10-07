860972cdb4093021d373f1c22dbabb79b12c2835
KRLiebel <79476734+KRLiebel@users.noreply.github.com>
2021-10-07 14:58:25 -0300

    Add files via upload

---
+++ Desafio03.py
@@ -0,0 +1,4 @@
+primeiroNumber = int(input('Digite um número:'))
+segundoNumber = int(input('Digite um segundo número:'))
+result = primeiroNumber+segundoNumber
+print('A soma entre os dois números é:', result)
