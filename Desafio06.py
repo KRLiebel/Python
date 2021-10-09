a1f6e9fd6232123e11663ba638696224bc8246b0
KRLiebel <79476734+KRLiebel@users.noreply.github.com>
2021-10-09 15:03:15 -0300

    Add files via upload

---
+++ Desafio06.py
@@ -0,0 +1,4 @@
+n = int(input('Escolha um número para ver o dobro, triplo e raiz quadrada: '))
+print('O dobro do número {} é igual a {}' .format(n, n*2))
+print('O triplo do número {} é {}' .format(n, n*3))
+print(f'A raiz quadrada do número {n} é {n ** (1/2)}')
