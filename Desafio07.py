ab4c1d71ce413d2410b8c344092d4d32b7428e26
KRLiebel <79476734+KRLiebel@users.noreply.github.com>
2021-10-09 15:14:29 -0300

    Add files via upload

---
+++ Desafio07.py
@@ -0,0 +1,9 @@
+nota1 = int(input('Digite a primeira nota: '))
+nota2 = int(input('Digite a segunda nota: '))
+media = (nota1 + nota2)
+final = (media/2)
+print(f'Essa é a média final do aluno: {final}')
+if (final > 7):
+    print('O aluno está aprovado!')
+else:
+    print('REPROVADO!2')
