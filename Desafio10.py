77dd03d493144d91cd77b663fe488e90d664d9ae
KRLiebel <79476734+KRLiebel@users.noreply.github.com>
2021-10-10 10:46:26 -0300

    Add files via upload

---
+++ Desafio10.py
@@ -0,0 +1,4 @@
+print('CONVERTENDO REAIS PARA DÓLARS.')
+real = float(input('Digite a quantide de dinheiro total que possui: '))
+dolar = (real / 5.51)
+print(f'Esse é o valor em real {real} convertido em dólar {dolar:.2f}')
