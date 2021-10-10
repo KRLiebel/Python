03cb2a25319118a512c599397315a1c69c70055f
KRLiebel <79476734+KRLiebel@users.noreply.github.com>
2021-10-10 12:58:40 -0300

    Add files via upload

---
+++ Desafio08.py
@@ -0,0 +1,5 @@
+metro = float(input('Informe a distância em metro: '))
+cen = int(metro * 100)
+print(f'A distância em metros {metro} convertido para centimetros é: {cen}')
+mili = int(cen * 1000)
+print(f'E convertendo centimetros {cen} para milimetro, o resultado vai ser: {mili}')
