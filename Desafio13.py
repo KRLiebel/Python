4f1ebc51f6b7556df7072c2c07ea758e687c4daa
KRLiebel <79476734+KRLiebel@users.noreply.github.com>
2021-10-10 14:52:55 -0300

    Add files via upload

---
+++ Desafio13.py
@@ -0,0 +1,4 @@
+print('Novo salário com aumento.')
+salario = float(input('Qual seu salário total? '))
+aumento = salario + (salario * 15 /100)
+print(f'Seu novo salário com um aumento de 15% é de {aumento}')
