a2f219db3b501828591c3658c1d25ed15112b215
KRLiebel <79476734+KRLiebel@users.noreply.github.com>
2021-10-12 18:14:38 -0300

    Add files via upload

---
+++ Desafio18.py
@@ -0,0 +1,8 @@
+import math
+a = int(input('Escolha qual será o ângulo: '))
+seno = math.sin(a)
+cos = math.cos(a)
+tan = math.tan(a)
+print(f'Seno: {seno:.2f}')
+print(f'Cosseno: {cos:.2f}')
+print(f'Tangente: {tan:.2f}')
